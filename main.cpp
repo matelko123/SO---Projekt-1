eb33d52f4214b6606ab38f2771cff6e65492fe29
Mateusz Gutowski <mateusz280798@gmail.com>
2020-12-11 22:06:37 +0100

    init file stream

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <fstream>
+#define fileName "psychologia-milosci.txt"
 
 using namespace std;
 
+void initFileStream(fstream &plik){
+    plik.open(fileName);
+
+    if(!plik || !plik.is_open()) {
+        cout<<"Unable to open the file"<<endl;
+        exit(1);
+    }
+}
+
 int main(){
-    cout<<"hello world"<<endl;
+    fstream plik;
+    initFileStream(plik);
+
+    plik.close();
     return 0;
 }
