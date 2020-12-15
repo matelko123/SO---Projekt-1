ec3fcec951d7fcfac32ff5d98a0e95c185873f0f
Mateusz Gutowski <mateusz280798@gmail.com>
2020-12-15 23:11:31 +0100

    hehehe

--- main.cpp
+++ main.cpp
@@ -2,15 +2,19 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <cctype>
+// #define orgFileName "lore.txt"
 #define orgFileName "psychologia-milosci.txt"
 #define endFileName "psychologia-milosci_done.txt"
+
+using namespace std;
 
 /* TODO
  ✅ Stworzyć funkcje do sprawdzania plikow a gdy plik nie istnieje to go tworzy
  * Poprawiać stringi aby nie miały znakow specjalnych
  */
 
-using namespace std;
+// -------------------------------------------------
 
 void throwError(int status=1, string msg="Some error"){
     cout<<msg<<endl;
@@ -26,9 +30,44 @@ void initFileStream(fstream &plik, string fileName){
     }
 }
 
-void prepareStringAplha(string &str){
-    // String to upper case
-    transform(str.begin(), str.end(),str.begin(), ::toupper);
+bool isCharAlpha(const char &c){
+    char znaki[] = {'.', ',', '\'', '"', '!', '?', '>', '<', '\\', '(', ')', ':', ';', '-', '_', '+', '=', '/'};
+    int length = sizeof(znaki)/sizeof(znaki[0]);
+
+    for(int i=0; i<length; i++)
+        if(c == znaki[i] || isdigit(c)) return false;
+        
+    return true;
+}
+
+bool isStringAlpha(const string &str){
+    for(int i=0; i<str.length(); i++)
+        if(!isCharAlpha(str[i])) return false;
+        
+    return true;
+}
+
+void trim(string &str){
+    size_t first = str.find_first_not_of(' ');
+    if (string::npos == first)
+        return;
+    
+    size_t last = str.find_last_not_of(' ');
+    str = str.substr(first, (last - first + 1));
+}
+
+// -------------------------------------------------
+
+void prepareString(string &str){
+    trim(str);                                                  // Trim string
+    transform(str.begin(), str.end(),str.begin(), ::toupper);   // String to upper case
+
+    int j=0;
+    while(isCharAlpha(str[j]) && j<str.length()) j++;
+
+    //cout<<str<<" "<<str.length()<<" "<<i<<" "<<j<<": "<<str.substr(i, j)<<endl;
+
+    str = str.substr(0, j);
 }
 
 void prepareFile(fstream &org, fstream &plik){
@@ -36,8 +75,8 @@ void prepareFile(fstream &org, fstream &plik){
 
     string tmp;
     while(org>>tmp){
-        prepareStringAplha(tmp);
-        plik<<tmp<<endl;
+        prepareString(tmp);
+        if(tmp.length() > 0) plik<<tmp<<endl;
     }
 }
 
@@ -46,6 +85,7 @@ int main(){
     initFileStream(org, orgFileName);
 
     fstream plik;
+    remove(endFileName);
     initFileStream(plik, endFileName);
 
     prepareFile(org, plik);
